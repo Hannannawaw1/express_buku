ff196079e0738813ec7a9bcfae04cfb44e48c86d
hannannawaw1 <hnawawi939@gmail.comgit confi --global user.name hannannawaw1git config --global user.email hnawawi939@gmail.com>
2021-10-21 15:08:18 +0700

    bismillah

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
 let mysql = require("mysql");
 
 let connection = mysql.createConnection({
+  /*
   host: "localhost",
+  */
+  host: "https://tkjbapinodejs.herokuapp.com/",
   user: "hannan",
   password: "123",
   database: "buku",
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 const postsRouter = require("./routes/posts");
 app.use("/api/posts", postsRouter); // use route posts di Express
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 /*
